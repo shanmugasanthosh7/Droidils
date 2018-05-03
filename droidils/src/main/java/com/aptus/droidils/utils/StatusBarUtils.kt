0d4119e497a1be21ba7090d8cad60fca17957f3e
santhosh <santhosh@luffalabs.com>
2018-05-04 00:10:52 +0530

    Add function changeStatusBarColor

--- droidils/src/main/java/com/aptus/droidils/utils/StatusBarUtils.kt
+++ droidils/src/main/java/com/aptus/droidils/utils/StatusBarUtils.kt
@@ -3,9 +3,12 @@
 package com.aptus.droidils.utils
 
 import android.app.Activity
-import android.content.Context
 import android.view.View
 import android.view.WindowManager
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
+
 
 fun Activity.fullScreen() {
     this.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
@@ -15,7 +18,13 @@ fun Activity.exitFullScreen() {
     this.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
 
-fun Activity.fullScreenImmersive(){
+fun Activity.fullScreenImmersive() {
     this.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+}
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun Activity.changeStatusBarColor(colorResId: Int) {
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    window.statusBarColor = ContextCompat.getColor(this, colorResId)
 }
