44cf810a3509d44d32702c0d30317b25c6c3b44a
Shanmugasanthosh <shanmugasanthosh@outlook.com>
2018-05-05 17:05:00 +0530

    Added travis.yml and descriptor.json

--- droidils/src/main/java/com/aptus/droidils/utils/ActivityUtils.kt
+++ droidils/src/main/java/com/aptus/droidils/utils/ActivityUtils.kt
@@ -3,8 +3,9 @@ package com.aptus.droidils.utils
 import android.app.Activity
 import android.content.Intent
 import android.util.DisplayMetrics
+import android.view.View
 
-fun Activity.urlActionView(url:String){
+fun Activity.urlActionView(url: String) {
     val browserIntent = Intent(Intent.ACTION_VIEW, url.toUri())
     startActivity(browserIntent)
 }
@@ -13,4 +14,8 @@ fun Activity.getDisplayMetrics(): DisplayMetrics {
     val displaymetrics = DisplayMetrics()
     windowManager.defaultDisplay.getMetrics(displaymetrics)
     return displaymetrics
+}
+
+fun Activity.SetOnClickListener(clickListener: View.OnClickListener, vararg view: View) {
+    view.forEach { it.setOnClickListener(clickListener) }
 }
